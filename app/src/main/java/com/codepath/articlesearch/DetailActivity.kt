666d1f2e3a08177a59c106a4cb6a1039cc740869
Bclayts71 <bren530.clayton@gmail.com>
2023-02-24 14:49:09 -0500

    Finished Lab5 code.

--- app/src/main/java/com/codepath/articlesearch/DetailActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/DetailActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.bumptech.glide.Glide
+
 
 private const val TAG = "DetailActivity"
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity(){
     private lateinit var mediaImageView: ImageView
     private lateinit var titleTextView: TextView
     private lateinit var bylineTextView: TextView
@@ -18,15 +21,16 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
+
         mediaImageView = findViewById(R.id.mediaImage)
         titleTextView = findViewById(R.id.mediaTitle)
         bylineTextView = findViewById(R.id.mediaByline)
         abstractTextView = findViewById(R.id.mediaAbstract)
 
-        val article = intent.getSerializableExtra(ARTICLE_EXTRA) as Article
+        val article = intent.getSerializableExtra(ARTICLE_EXTRA) as DisplayArticle
 
-        titleTextView.text = article.headline?.main
-        bylineTextView.text = article.byline?.original
+        titleTextView.text = article.headline
+        bylineTextView.text = article.byline
         abstractTextView.text = article.abstract
 
         Glide.with(this)
